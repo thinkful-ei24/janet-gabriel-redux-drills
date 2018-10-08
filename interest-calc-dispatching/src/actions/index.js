e38477b21f81427ddb814172dc6f54fec9af2b1a
Gabriel Morris <gabemorris@outlook.com>
2018-10-08 08:19:28 -0700

    finished dispatching

--- interest-calc-dispatching/src/actions/index.js
+++ interest-calc-dispatching/src/actions/index.js
@@ -1,19 +1,17 @@
 export const SET_PRINCIPAL = 'SET_PRINCIPAL';
 export const setPrincipal = principal => ({
-    type: SET_PRINCIPAL,
-    principal
+  type: SET_PRINCIPAL,
+  principal
 });
-
 
 export const SET_INTEREST = 'SET_INTEREST';
 export const setInterest = interest => ({
-    type: SET_INTEREST,
-    interest
+  type: SET_INTEREST,
+  interest
 });
-
 
 export const SET_YEARS = 'SET_YEARS';
 export const setYears = years => ({
-    type: SET_YEARS,
-    years
+  type: SET_YEARS,
+  years
 });
